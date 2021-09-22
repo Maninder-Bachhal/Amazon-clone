b642529191af5c571e2f2c0b2ab0b6abdaf00bb6
Maninder <maninderbachhal25@gmailcom>
2021-09-23 01:58:41 +0530

    Designed Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import './App.css';
 import Header from "./Header";
+import Home from "./Home";
 function App() {
   return (
     <div className="app">
-      <h1>
-        Hey,let's Build Amazon-clone!
-      </h1>
       <Header/>
+      <Home/>
     </div>
   );
 }
