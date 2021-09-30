295fbdb852af6185c8cb70b27080b3a36a4586f6
Maninder <maninderbachhal25@gmailcom>
2021-09-30 22:14:52 +0530

    Designed Login Page

--- src/App.js
+++ src/App.js
@@ -4,18 +4,23 @@ import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
+import Login from "./Login";
 
 function App() {
   return (
     <Router>
       <div className="app">
-      <Header/>
         <Switch>
+          <Route path="/login">
+            <h1><Login /></h1>
+          </Route>
           <Route path="/checkout">
-            <Checkout/>
+            <Header/>
+            <Checkout />
           </Route>
           {/* This is default route.Make sure its always written all these at end . */}
           <Route path="/">
+            <Header/>
             <Home />
           </Route>
         </Switch>
