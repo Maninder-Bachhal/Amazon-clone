228ad11b5be50804ce0f9e474481a913fec49de1
Maninder <maninderbachhal25@gmailcom>
2021-10-04 23:45:30 +0530

    user authentication

--- src/Login.js
+++ src/Login.js
@@ -85,7 +85,6 @@ function Login() {
 
     const register = e => {
         e.preventDefault();
-
         auth
             .createUserWithEmailAndPassword(email, password)
             .then((auth) => {
